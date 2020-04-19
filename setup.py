d724f6ff9cadefe6b3e981a72b85ae261833196d
lizixi <lizixi2006@outlook.com>
2020-04-19 21:42:29 +0800

    version0.0.3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 __project__='ISS_now'
-__version__='0.0.2'
+__version__='0.0.3'
 __description__='''a python moudle to display the coordinates of the international space station'''
 __packages__=['ISS']
 __author__ = "lzx-047"
